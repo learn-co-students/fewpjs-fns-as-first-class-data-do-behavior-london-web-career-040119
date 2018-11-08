572e025b64abf2698d6c022b4ba62712fa0769c6
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-11-08 14:03:57 -0500

    Transfers outline/rough draft

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,14 @@
 describe("index.js", () => {
-  it("exists", () => {
-    expect(greet).to.exist
+  it("Returns Good Morning before 12pm", () => {
+    expect(greet(8)).to.equal('Good Morning')
+  })
+  it("Returns Good Afternoon between 12pm and 5pm ", () => {
+    expect(greet(14)).to.equal('Good Afternoon')
+  })
+  it("Returns Good Evening after 5pm", () => {
+    expect(greet(19)).to.equal('Good Evening')
+  })
+  it("Returns Hello with other input", () => {
+    expect(greet("Random Input")).to.equal('Hello')
   })
 })
