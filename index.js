d184b8ae5ebf894ccf71ba7883e3e5dca02e6915
github username <github email address>
2019-05-13 16:18:02 +0100

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg) {
+  document.getElementById("greeting").textContent = msg;
+}
+
+function greet(timeStr) {
+  /* It would be a good idea to make sure hour isn't less than 0 or greater
+   * than 24...perhaps "throwing an Error" if one of these impossible cases
+   * happens.
+   *
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error#Throwing_a_generic_error
+   *
+   */
+  const hour = parseInt(timeStr, 10);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
