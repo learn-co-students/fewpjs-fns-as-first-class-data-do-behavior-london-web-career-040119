7836a9b2b73bb4f8382ead422d1fab83223b8bb0
github username <github email address>
2019-05-13 16:05:07 +0100

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg) {
+  document.getElementById("greeting").textContent = msg;
+}
+
+function greet(timeString) {
+
+  let hourString = timeString.substring(0,2)
+  let hourInt = parseInt(hourString)
+
+  if (hourInt < 13) return "Good Morning"
+  if (hourInt < 18) return "Good Afternoon"
+  return "Good Evening"
+}
