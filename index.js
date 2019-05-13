89693b3aa486be913b2751e7f7a636c7442f61af
Ivan Szebenszki <szib77@gmail.com>
2019-05-13 14:03:52 +0100

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById('time').value;
+  displayMessage(greet(timeString));
 }
 
-/* End Given Code, don't edit above here...*/
-
+/* End Given Code, don't edit above here... */
 /* Write your implementation of greet() */
+function greet(timeString) {
+  const hour = timeString.split(':')[0];
+  if (hour < 12) {
+    return 'Good Morning';
+  } if (hour > 17) {
+    return 'Good Evening';
+  }
+  return 'Good Afternoon';
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  const greetingEl = document.querySelector('#greeting');
+  greetingEl.textContent = message;
+}
