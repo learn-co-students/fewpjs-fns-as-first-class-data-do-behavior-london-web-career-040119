c3566845d1962a39fe9538ff471493a8fa59480a
Nahit-1 <nahit86@outlook.com>
2019-05-14 08:31:13 +0100

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  const hour = parseInt(string, 10)
+  if ( hour < 12 ) return "Good Morning"
+  if ( hour > 17 ) return "Good Evening"
+  return "Good Afternoon"
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg) {
+  document.getElementById("greeting").textContent = msg;
+}
