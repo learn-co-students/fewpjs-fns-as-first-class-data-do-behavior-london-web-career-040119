40c9e9fc9f4ef65c673b3d46dec6199a35cd8091
Ann Duong <39737217+thuyanduong-flatiron@users.noreply.github.com>
2020-04-13 22:46:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* Given Code, don't edit */
 
-document.querySelector("button").addEventListener(handleClick);
+document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
   const timeString = document.getElementById('time').value
