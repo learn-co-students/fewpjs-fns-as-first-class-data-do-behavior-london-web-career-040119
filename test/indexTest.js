346bdbdc32d9076b994cc351948144d64d4f2445
ihollander <ianholla@gmail.com>
2021-06-22 07:34:39 -0400

    Replace innerText with textContent

--- test/indexTest.js
+++ test/indexTest.js
@@ -15,7 +15,7 @@ describe("index.js", () => {
     it("updates the DOM with an appropriate message", () => {
       let testContent = "TEST"
       displayMessage(testContent)
-      let content = document.getElementById("greeting").innerText
+      let content = document.getElementById("greeting").textContent
       expect(content).to.equal(testContent)
     })
   })
